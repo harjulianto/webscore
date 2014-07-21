f852fa4052021394958468471d12abf2ea75574d
harjulianto <feriharjulianto@gmail.com>
2014-07-21 12:06:45 +0700

    modified conf.php

--- setting/conf.php
+++ setting/conf.php
@@ -5,6 +5,6 @@ $pass = 'juventus';
 $dbname = 'webscore';
 $connect = mysql_connect($host, $user, $pass) or die(mysql_error());
 $dbselect = mysql_select_db($dbname);
-
+//test
 ?>
                             
